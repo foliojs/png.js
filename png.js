eec0faa219f670dc4df678a108c331f0573bc33f
meltingice <meltingice8917@gmail.com>
2012-04-06 18:29:12 -0400

    Removed all browser/canvas dependent code and switched to built-in zlib

--- png.js
+++ png.js
@@ -1,47 +1,38 @@
+
+/*
+# MIT LICENSE
+# Copyright (c) 2011 Devon Govett
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+# software and associated documentation files (the "Software"), to deal in the Software 
+# without restriction, including without limitation the rights to use, copy, modify, merge, 
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
+# to whom the Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or 
+# substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 (function() {
+  var PNG, fs, zlib;
 
-  /*
-  # MIT LICENSE
-  # Copyright (c) 2011 Devon Govett
-  # 
-  # Permission is hereby granted, free of charge, to any person obtaining a copy of this 
-  # software and associated documentation files (the "Software"), to deal in the Software 
-  # without restriction, including without limitation the rights to use, copy, modify, merge, 
-  # publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons 
-  # to whom the Software is furnished to do so, subject to the following conditions:
-  # 
-  # The above copyright notice and this permission notice shall be included in all copies or 
-  # substantial portions of the Software.
-  # 
-  # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
-  # BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-  # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-  # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-  # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-  */
+  fs = require('fs');
 
-  var PNG;
+  zlib = require('zlib');
 
-  PNG = (function() {
-    var APNG_BLEND_OP_OVER, APNG_BLEND_OP_SOURCE, APNG_DISPOSE_OP_BACKGROUND, APNG_DISPOSE_OP_NONE, APNG_DISPOSE_OP_PREVIOUS, makeImage, scratchCanvas, scratchCtx;
+  exports.PNG = PNG = (function() {
+    var APNG_BLEND_OP_OVER, APNG_BLEND_OP_SOURCE, APNG_DISPOSE_OP_BACKGROUND, APNG_DISPOSE_OP_NONE, APNG_DISPOSE_OP_PREVIOUS;
 
-    PNG.load = function(url, canvas, callback) {
-      var xhr;
-      var _this = this;
-      if (typeof canvas === 'function') callback = canvas;
-      xhr = new XMLHttpRequest;
-      xhr.open("GET", url, true);
-      xhr.responseType = "arraybuffer";
-      xhr.onload = function() {
-        var data, png;
-        data = new Uint8Array(xhr.response || xhr.mozResponseArrayBuffer);
-        png = new PNG(data);
-        if (typeof (canvas != null ? canvas.getContext : void 0) === 'function') {
-          png.render(canvas);
-        }
-        return typeof callback === "function" ? callback(png) : void 0;
-      };
-      return xhr.send(null);
+    PNG.load = function(path) {
+      var file;
+      file = fs.readFileSync(path);
+      return new PNG(file);
     };
 
     APNG_DISPOSE_OP_NONE = 0;
@@ -55,7 +46,7 @@
     APNG_BLEND_OP_OVER = 1;
 
     function PNG(data) {
-      var chunkSize, colors, delayDen, delayNum, frame, i, index, key, section, short, text, _ref;
+      var chunkSize, colors, frame, i, section, short, _ref;
       this.data = data;
       this.pos = 8;
       this.palette = [];
@@ -84,41 +75,12 @@
             this.filterMethod = this.data[this.pos++];
             this.interlaceMethod = this.data[this.pos++];
             break;
-          case 'acTL':
-            this.animation = {
-              numFrames: this.readUInt32(),
-              numPlays: this.readUInt32() || Infinity,
-              frames: []
-            };
-            break;
           case 'PLTE':
             this.palette = this.read(chunkSize);
             break;
-          case 'fcTL':
-            if (frame) this.animation.frames.push(frame);
-            this.pos += 4;
-            frame = {
-              width: this.readUInt32(),
-              height: this.readUInt32(),
-              xOffset: this.readUInt32(),
-              yOffset: this.readUInt32()
-            };
-            delayNum = this.readUInt16();
-            delayDen = this.readUInt16() || 100;
-            frame.delay = 1000 * delayNum / delayDen;
-            frame.disposeOp = this.data[this.pos++];
-            frame.blendOp = this.data[this.pos++];
-            frame.data = [];
-            break;
           case 'IDAT':
-          case 'fdAT':
-            if (section === 'fdAT') {
-              this.pos += 4;
-              chunkSize -= 4;
-            }
-            data = (frame != null ? frame.data : void 0) || this.imgData;
             for (i = 0; 0 <= chunkSize ? i < chunkSize : i > chunkSize; 0 <= chunkSize ? i++ : i--) {
-              data.push(this.data[this.pos++]);
+              this.imgData.push(this.data[this.pos++]);
             }
             break;
           case 'tRNS':
@@ -140,14 +102,7 @@
                 this.transparency.rgb = this.read(chunkSize);
             }
             break;
-          case 'tEXt':
-            text = this.read(chunkSize);
-            index = text.indexOf(0);
-            key = String.fromCharCode.apply(String, text.slice(0, index));
-            this.text[key] = String.fromCharCode.apply(String, text.slice(index + 1));
-            break;
           case 'IEND':
-            if (frame) this.animation.frames.push(frame);
             this.colors = (function() {
               switch (this.colorType) {
                 case 0:
@@ -170,7 +125,7 @@
                   return 'DeviceRGB';
               }
             }).call(this);
-            this.imgData = new Uint8Array(this.imgData);
+            this.imgData = new Buffer(this.imgData);
             return;
           default:
             this.pos += chunkSize;
@@ -205,88 +160,88 @@
       return b1 | b2;
     };
 
-    PNG.prototype.decodePixels = function(data) {
-      var byte, col, filter, i, left, length, p, pa, paeth, pb, pc, pixelBytes, pixels, pos, row, rowData, s, scanlineLength, upper, upperLeft, _ref;
-      if (data == null) data = this.imgData;
-      if (data.length === 0) return [];
-      data = new FlateStream(data);
-      data = data.getBytes();
-      pixelBytes = this.pixelBitlength / 8;
-      scanlineLength = pixelBytes * this.width;
-      row = 0;
-      pixels = [];
-      length = data.length;
-      pos = 0;
-      while (pos < length) {
-        filter = data[pos++];
-        i = 0;
-        rowData = [];
-        switch (filter) {
-          case 0:
-            while (i < scanlineLength) {
-              rowData[i++] = data[pos++];
-            }
-            break;
-          case 1:
-            while (i < scanlineLength) {
-              byte = data[pos++];
-              left = i < pixelBytes ? 0 : rowData[i - pixelBytes];
-              rowData[i++] = (byte + left) % 256;
-            }
-            break;
-          case 2:
-            while (i < scanlineLength) {
-              byte = data[pos++];
-              col = (i - (i % pixelBytes)) / pixelBytes;
-              upper = row === 0 ? 0 : pixels[row - 1][col][i % pixelBytes];
-              rowData[i++] = (upper + byte) % 256;
-            }
-            break;
-          case 3:
-            while (i < scanlineLength) {
-              byte = data[pos++];
-              col = (i - (i % pixelBytes)) / pixelBytes;
-              left = i < pixelBytes ? 0 : rowData[i - pixelBytes];
-              upper = row === 0 ? 0 : pixels[row - 1][col][i % pixelBytes];
-              rowData[i++] = (byte + Math.floor((left + upper) / 2)) % 256;
-            }
-            break;
-          case 4:
-            while (i < scanlineLength) {
-              byte = data[pos++];
-              col = (i - (i % pixelBytes)) / pixelBytes;
-              left = i < pixelBytes ? 0 : rowData[i - pixelBytes];
-              if (row === 0) {
-                upper = upperLeft = 0;
-              } else {
-                upper = pixels[row - 1][col][i % pixelBytes];
-                upperLeft = col === 0 ? 0 : pixels[row - 1][col - 1][i % pixelBytes];
+    PNG.prototype.decodePixels = function(fn) {
+      var _this = this;
+      return zlib.inflate(this.imgData, function(err, data) {
+        var byte, col, filter, i, left, length, p, pa, paeth, pb, pc, pixelBytes, pixels, pos, row, rowData, s, scanlineLength, upper, upperLeft, _ref;
+        if (err) throw err;
+        pixelBytes = _this.pixelBitlength / 8;
+        scanlineLength = pixelBytes * _this.width;
+        row = 0;
+        pixels = [];
+        length = data.length;
+        pos = 0;
+        while (pos < length) {
+          filter = data[pos++];
+          i = 0;
+          rowData = [];
+          switch (filter) {
+            case 0:
+              while (i < scanlineLength) {
+                rowData[i++] = data[pos++];
               }
-              p = left + upper - upperLeft;
-              pa = Math.abs(p - left);
-              pb = Math.abs(p - upper);
-              pc = Math.abs(p - upperLeft);
-              if (pa <= pb && pa <= pc) {
-                paeth = left;
-              } else if (pb <= pc) {
-                paeth = upper;
-              } else {
-                paeth = upperLeft;
+              break;
+            case 1:
+              while (i < scanlineLength) {
+                byte = data[pos++];
+                left = i < pixelBytes ? 0 : rowData[i - pixelBytes];
+                rowData[i++] = (byte + left) % 256;
               }
-              rowData[i++] = (byte + paeth) % 256;
-            }
-            break;
-          default:
-            throw new Error("Invalid filter algorithm: " + filter);
+              break;
+            case 2:
+              while (i < scanlineLength) {
+                byte = data[pos++];
+                col = (i - (i % pixelBytes)) / pixelBytes;
+                upper = row === 0 ? 0 : pixels[row - 1][col][i % pixelBytes];
+                rowData[i++] = (upper + byte) % 256;
+              }
+              break;
+            case 3:
+              while (i < scanlineLength) {
+                byte = data[pos++];
+                col = (i - (i % pixelBytes)) / pixelBytes;
+                left = i < pixelBytes ? 0 : rowData[i - pixelBytes];
+                upper = row === 0 ? 0 : pixels[row - 1][col][i % pixelBytes];
+                rowData[i++] = (byte + Math.floor((left + upper) / 2)) % 256;
+              }
+              break;
+            case 4:
+              while (i < scanlineLength) {
+                byte = data[pos++];
+                col = (i - (i % pixelBytes)) / pixelBytes;
+                left = i < pixelBytes ? 0 : rowData[i - pixelBytes];
+                if (row === 0) {
+                  upper = upperLeft = 0;
+                } else {
+                  upper = pixels[row - 1][col][i % pixelBytes];
+                  upperLeft = col === 0 ? 0 : pixels[row - 1][col - 1][i % pixelBytes];
+                }
+                p = left + upper - upperLeft;
+                pa = Math.abs(p - left);
+                pb = Math.abs(p - upper);
+                pc = Math.abs(p - upperLeft);
+                if (pa <= pb && pa <= pc) {
+                  paeth = left;
+                } else if (pb <= pc) {
+                  paeth = upper;
+                } else {
+                  paeth = upperLeft;
+                }
+                rowData[i++] = (byte + paeth) % 256;
+              }
+              break;
+            default:
+              throw new Error("Invalid filter algorithm: " + filter);
+          }
+          s = [];
+          for (i = 0, _ref = rowData.length; 0 <= _ref ? i < _ref : i > _ref; i += pixelBytes) {
+            s.push(rowData.slice(i, i + pixelBytes));
+          }
+          pixels.push(s);
+          row += 1;
         }
-        s = [];
-        for (i = 0, _ref = rowData.length; 0 <= _ref ? i < _ref : i > _ref; i += pixelBytes) {
-          s.push(rowData.slice(i, i + pixelBytes));
-        }
-        pixels.push(s);
-        row += 1;
-      }
-      return pixels;
+        return fn(pixels);
+      });
     };
 
     PNG.prototype.decodePalette = function() {
@@ -337,96 +292,8 @@
       }
     };
 
-    scratchCanvas = document.createElement('canvas');
-
-    scratchCtx = scratchCanvas.getContext('2d');
-
-    makeImage = function(imageData) {
-      var img;
-      scratchCtx.width = imageData.width;
-      scratchCtx.height = imageData.height;
-      scratchCtx.clearRect(0, 0, imageData.width, imageData.height);
-      scratchCtx.putImageData(imageData, 0, 0);
-      img = new Image;
-      img.src = scratchCanvas.toDataURL();
-      return img;
-    };
-
-    PNG.prototype.decodeFrames = function(ctx) {
-      var frame, i, imageData, pixels, _len, _ref, _results;
-      if (!this.animation) return;
-      _ref = this.animation.frames;
-      _results = [];
-      for (i = 0, _len = _ref.length; i < _len; i++) {
-        frame = _ref[i];
-        imageData = ctx.createImageData(frame.width, frame.height);
-        pixels = this.decodePixels(new Uint8Array(frame.data));
-        this.copyToImageData(imageData, pixels);
-        frame.imageData = imageData;
-        _results.push(frame.image = makeImage(imageData));
-      }
-      return _results;
-    };
-
-    PNG.prototype.renderFrame = function(ctx, number) {
-      var frame, frames, prev;
-      frames = this.animation.frames;
-      frame = frames[number];
-      prev = frames[number - 1];
-      if (number === 0) ctx.clearRect(0, 0, this.width, this.height);
-      if ((prev != null ? prev.disposeOp : void 0) === APNG_DISPOSE_OP_BACKGROUND) {
-        ctx.clearRect(prev.xOffset, prev.yOffset, prev.width, prev.height);
-      } else if ((prev != null ? prev.disposeOp : void 0) === APNG_DISPOSE_OP_PREVIOUS) {
-        ctx.putImageData(prev.imageData, prev.xOffset, prev.yOffset);
-      }
-      if (frame.blendOp === APNG_BLEND_OP_SOURCE) {
-        ctx.clearRect(frame.xOffset, frame.yOffset, frame.width, frame.height);
-      }
-      return ctx.drawImage(frame.image, frame.xOffset, frame.yOffset);
-    };
-
-    PNG.prototype.animate = function(ctx) {
-      var doFrame, frameNumber, frames, numFrames, numPlays, _ref;
-      var _this = this;
-      frameNumber = 0;
-      _ref = this.animation, numFrames = _ref.numFrames, frames = _ref.frames, numPlays = _ref.numPlays;
-      return (doFrame = function() {
-        var f, frame;
-        f = frameNumber++ % numFrames;
-        frame = frames[f];
-        _this.renderFrame(ctx, f);
-        if (numFrames > 1 && frameNumber / numFrames < numPlays) {
-          return _this.animation._timeout = setTimeout(doFrame, frame.delay);
-        }
-      })();
-    };
-
-    PNG.prototype.stopAnimation = function() {
-      var _ref;
-      return clearTimeout((_ref = this.animation) != null ? _ref._timeout : void 0);
-    };
-
-    PNG.prototype.render = function(canvas) {
-      var ctx, data;
-      if (canvas._png) canvas._png.stopAnimation();
-      canvas._png = this;
-      canvas.width = this.width;
-      canvas.height = this.height;
-      ctx = canvas.getContext("2d");
-      if (this.animation) {
-        this.decodeFrames(ctx);
-        return this.animate(ctx);
-      } else {
-        data = ctx.createImageData(this.width, this.height);
-        this.copyToImageData(data, this.decodePixels());
-        return ctx.putImageData(data, 0, 0);
-      }
-    };
-
     return PNG;
 
   })();
-
-  window.PNG = PNG;
 
 }).call(this);
